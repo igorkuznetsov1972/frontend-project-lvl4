63bfc4326ddbb826c39d709cd1117ac04a688f97
Igor <ikk@mail.ru>
2022-03-25 13:43:52 +0300

    add store provider

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,14 @@
 // @ts-check
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './components/chat/store/store.js';
 import App from './components/App.jsx';
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.querySelector('#chat'),
 );
 /* import 'core-js/stable/index.js';
