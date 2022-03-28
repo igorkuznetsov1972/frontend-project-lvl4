ec1d9367c960b164739252d529e1d8b8b3a4fa91
Igor <ikk@mail.ru>
2022-03-28 17:53:19 +0300

    add ru-Ru locale

---
+++ src/assets/locales/ru.js
@@ -0,0 +1,16 @@
+export default {
+  translation: {
+    username: 'Ваш ник',
+    password: 'Пароль',
+    login: 'Войти',
+    logout: 'Выйти',
+    'not registred': 'Нет аккаунта?',
+    signup: 'Регистрация',
+    channels: 'Каналы',
+    channel: 'канал'
+    delete: 'Удалить',
+    rename: 'Переименовать',
+    posts: 'Посты',
+  ,
+  },
+};
