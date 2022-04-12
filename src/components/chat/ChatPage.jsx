a5b8e92e6c3550f202759f5a7e3d069a68512d60
Igor <ikk@mail.ru>
2022-04-12 12:03:36 +0300

    add sockets and new message feature

--- src/components/chat/ChatPage.jsx
+++ src/components/chat/ChatPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {
   Container, Row, Col, Nav, Button, Spinner, Alert,
@@ -8,19 +8,28 @@ import {
 } from 'formik';
 import _ from 'lodash';
 import { fetchChat, changeCurrentChannel } from './slices/chatSlice';
-import useChat from '../../hooks/useChat'
+import useChat from '../../hooks/useChat';
+import useAuth from '../../hooks/useAuth';
 
 export default () => {
   const dispatch = useDispatch();
-  const { sendMessage, addChannel, renameChannel, removeChannel } = useChat();
+
+  const {
+    sendMessage,
+  } = useChat();
+  const auth = useAuth();
+  const currentUser = auth.user.username;
 
   useEffect(() => {
     dispatch(fetchChat());
   }, []);
 
+  const messageRef = useRef(null);
+
   const handleClick = (id) => (e) => {
     e.preventDefault();
     dispatch(changeCurrentChannel(id));
+    messageRef.current.focus();
   };
 
   const {
@@ -86,41 +95,53 @@ export default () => {
                   </span>
                 </div>
                 <div id="messages-box" className="chat-messages overflow-auto px-5">
-                  {messages
-                    .filter(({ channelId }) => channelId === currentChannelId)
-                    .map(({ username, body, id }) => (
-                      <div className="text-break mb-2" key={id}>
-                        <b>{username}</b>
-                        :
-                        {body}
-                      </div>
-                    ))}
+                  {currentChannelMessages.map(({ username, body, id }) => (
+                    <div className="text-break mb-2" key={id}>
+                      <b>{username}</b>
+                      :
+                      {body}
+                    </div>
+                  ))}
                 </div>
                 <div className="mt-auto px-5 py-3">
                   <Formik
                     initialValues={{
                       newMessage: '',
                     }}
-                    onSubmit={(values) => sendMessage(values.newMessage)}
+                    onSubmit={({ newMessage }, actions) => {
+                      sendMessage({
+                        body: newMessage, channelId: currentChannelId, username: currentUser,
+                      });
+                      actions.resetForm();
+                      messageRef.current.focus();
+                    }}
                   >
-                    <Form className="py-1 border rounded-2">
-                      <div className="input-group has-validation">
-                        <Field
-                          id="newMessage"
-                          name="newMessage"
-                          type="text"
-                          aria-label="Новое сообщение"
-                          placeholder="Введите сообщение..."
-                          className="border-0 p-0 ps-2 form-control"
-                        />
-                        <div className="input-group-append">
-                          <button type="submit" className="btn btn-group-vertical" disabled="">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor"><path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" /></svg>
-                            <span className="visually-hidden">Отправить</span>
-                          </button>
+                    { (props) => (
+                      <Form
+                        onSubmit={props.handleSubmit}
+                        onChange={props.handleChange}
+                        className="py-1 border rounded-2"
+                      >
+                        <div className="input-group has-validation">
+                          <Field
+                            autoFocus
+                            innerRef={messageRef}
+                            id="newMessage"
+                            name="newMessage"
+                            type="text"
+                            aria-label="Новое сообщение"
+                            placeholder="Введите сообщение..."
+                            className="border-0 p-0 ps-2 form-control"
+                          />
+                          <div className="input-group-append">
+                            <button type="submit" className="btn btn-group-vertical" disabled="">
+                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor"><path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" /></svg>
+                              <span className="visually-hidden">Отправить</span>
+                            </button>
+                          </div>
                         </div>
-                      </div>
-                    </Form>
+                      </Form>
+                    )}
                   </Formik>
                 </div>
               </div>
--- src/hooks/useChat.js
+++ src/hooks/useChat.js
@@ -1,43 +1,44 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef } from 'react';
 import { io } from 'socket.io-client';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import {
   addChannel, removeChannel, renameChannel, addMessage,
 } from '../components/chat/slices/chatSlice.js';
 
-const useChat = () => {
+export default () => {
   const dispatch = useDispatch();
-  const { current: socket } = useRef(io());
+  const socketRef = useRef(null);
 
   useEffect(() => {
-    socket.on('newMessage', (message) => dispatch(addMessage(message)));
-    socket.on('newChannel', (channel) => dispatch(addChannel(channel)));
-    socket.on('removeChannel', ({ id }) => dispatch(removeChannel(id)));
-    socket.on('renameChannel', ({ id, name }) => dispatch(renameChannel({ id, name })));
+    socketRef.current = io();
+    socketRef.current.on('newMessage', (message) => dispatch(addMessage(message)));
+    socketRef.current.on('newChannel', (channel) => dispatch(addChannel(channel)));
+    socketRef.current.on('removeChannel', ({ id }) => dispatch(removeChannel(id)));
+    socketRef.current.on('renameChannel', ({ id, name }) => dispatch(renameChannel({ id, name })));
   }, []);
 
   const sendMessage = (message) => {
-    socket.emit('newMessage', message, (response) => {
+    socketRef.current.emit('newMessage', message, (response) => {
       console.log(response.status);
     });
   };
-  const addChannel = (name) => {
-    socket.emit('newChannel', { name }, (response) => {
+  const newChannel = (name) => {
+    socketRef.current.emit('newChannel', { name }, (response) => {
       console.log(response.status);
     });
   };
-  const renameChannel = (id, name) => {
-    socket.emit('renameChannel', { id, name }, (response) => {
+  const editChannel = (id, name) => {
+    socketRef.current.emit('renameChannel', { id, name }, (response) => {
       console.log(response.status);
     });
   };
-  const removeChannel = (id) => {
-    socket.emit('removeChannel', { id }, (response) => {
+  const deleteChannel = (id) => {
+    socketRef.current.emit('removeChannel', { id }, (response) => {
       console.log(response.status);
     });
   };
   return {
- sendMessage, addChannel, renameChannel, removeChannel
+    sendMessage, newChannel, editChannel, deleteChannel,
+  };
 };
-};
-export default useChat;
+// export default useChat;
