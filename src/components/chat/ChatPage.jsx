0dde41854871aea73505fc7cef93cb9c97147910
Igor <ikk@mail.ru>
2022-04-15 19:22:28 +0300

    correct container attributes to allow for large text

--- src/components/chat/ChatPage.jsx
+++ src/components/chat/ChatPage.jsx
@@ -63,7 +63,7 @@ export default () => {
       );
     case 'fulfilled':
       return (
-        <Container fluid className="h-100 my-4 overflow-hidden rounded shadow  text-truncate">
+        <Container fluid className="h-100 my-4 overflow-hidden rounded shadow">
           <Row className="h-100 bg-white flex-md-row">
             <Col xs={4} md={2} className="border-end pt-5 px-0 bg-light">
               <div className="d-flex justify-content-between mb-2 ps-4 pe-2">
@@ -134,7 +134,7 @@ export default () => {
                   ))) }
               </Nav>
             </Col>
-            <Col className="col p-0 h-100">
+            <Col className="p-0 h-100">
               <div className="d-flex flex-column h-100">
                 <div className="bg-light mb-4 p-3 shadow-sm small">
                   <p className="m-0">
