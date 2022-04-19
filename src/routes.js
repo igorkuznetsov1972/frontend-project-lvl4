358eade1873185fb4941c827bd6a55d63f783863
Igor <ikk@mail.ru>
2022-04-19 17:26:25 +0300

    remove unneeded routes

--- src/routes.js
+++ src/routes.js
@@ -5,9 +5,6 @@ const prefix = 'api/v1';
 
 export default {
   chatPath: () => [host, prefix, 'data'].join('/'),
-  channelsPath: () => [host, prefix, 'channels'].join('/'),
-  channelPath: (id) => [host, prefix, 'channels', id].join('/'),
-  channelMessagesPath: (id) => [host, prefix, 'channels', id, 'messages'].join('/'),
   loginPath: () => [host, prefix, 'login'].join('/'),
   signupPath: () => [host, prefix, 'signup'].join('/'),
 };
