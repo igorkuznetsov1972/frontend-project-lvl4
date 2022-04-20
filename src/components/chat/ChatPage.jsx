69babb167d6a77a7d3081ddff258a7aa0312b962
Igor <ikk@mail.ru>
2022-04-20 18:11:04 +0300

    correct channels typo

--- src/components/chat/ChatPage.jsx
+++ src/components/chat/ChatPage.jsx
@@ -76,7 +76,7 @@ export default () => {
           <Row className="h-100 bg-white flex-md-row">
             <Col xs={4} md={2} className="border-end pt-5 px-0 bg-light">
               <div className="d-flex justify-content-between mb-2 ps-4 pe-2">
-                <span>{t('çhannels')}</span>
+                <span>{t('channels')}</span>
                 <button type="button" className="p-0 text-primary btn btn-group-vertical" onClick={() => setNewChannelModalShow(true)}>
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor">
                     <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
