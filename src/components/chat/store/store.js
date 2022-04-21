6fee9df4f40b4802854bc80c98d36ad9caed11b7
Igor <ikk@mail.ru>
2022-04-21 16:27:52 +0300

    disable default export preference

--- src/components/chat/store/store.js
+++ src/components/chat/store/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import chatSliceReducer from '../slices/chatSlice.js';
 
+// eslint-disable-next-line import/prefer-default-export
 export const store = configureStore({
   reducer: {
     chat: chatSliceReducer,
