e530f3b920e9299629ef20f18af4866e66fc0ce3
Igor <ikk@mail.ru>
2022-04-21 17:05:16 +0300

    add entrypoint

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -6,6 +6,7 @@ const path = require('path');
 const mode = process.env.NODE_ENV || 'development';
 
 module.exports = {
+  entry: './src/index.jsx',
   mode,
   resolve: {
     extensions: ['.js', '.jsx'],
