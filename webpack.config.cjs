688ea05c0066a631e7c67415313525c333322a26
Igor <ikk@mail.ru>
2022-04-22 17:05:36 +0300

    modify entrypoint

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -6,7 +6,7 @@ const path = require('path');
 const mode = process.env.NODE_ENV || 'development';
 
 module.exports = {
-  entry: './src/index.jsx',
+  entry: '/src/index.jsx',
   mode,
   resolve: {
     extensions: ['.js', '.jsx'],
