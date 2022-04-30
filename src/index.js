8ebcb8e6562dd442367a39a00be0c77f6a65227e
Igor <ikk@mail.ru>
2022-04-30 11:16:16 +0300

    make init asyncronous

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import ReactDOM from 'react-dom';
 import { io } from 'socket.io-client';
 import init from './init.jsx';
 
-const socket = io();
-console.log(socket)
-ReactDOM.render(
-  init(socket),
-  document.getElementById('chat'),
-);
+// const socket = io();
+// console.log(socket)
+const app = async () => {
+  const socket = io();
+  const vdom = await init(socket);
+  ReactDOM.render(
+    vdom,
+    document.getElementById('chat'),
+  );
+};
+app();
